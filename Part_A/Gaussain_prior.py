3b2984fad7b1c3d08ce0d116a2e26cd9efdca8f0
meriams <114403637+meriams@users.noreply.github.com>
2024-02-22 13:28:40 +0100

    Files to folder Part_A

---
+++ Part_A/Gaussain_prior.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.distributions as td
+
+
+
+# The gaussain prior class
+class GaussianPrior(nn.Module):
+    def __init__(self, M):
+        """
+        Define a Gaussian prior distribution with zero mean and unit variance.
+
+                Parameters:
+        M: [int] 
+           Dimension of the latent space.
+        """
+        super(GaussianPrior, self).__init__()
+        self.M = M
+        self.mean = nn.Parameter(torch.zeros(self.M), requires_grad=False)
+        self.std = nn.Parameter(torch.ones(self.M), requires_grad=False)
+
+    def forward(self):
+        """
+        Return the prior distribution.
+
+        Returns:
+        prior: [torch.distributions.Distribution]
+        """
+        return td.Independent(td.Normal(loc=self.mean, scale=self.std), 1)
