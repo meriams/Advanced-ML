173759177e67fd2fb08b880d3bc4198b6b959625
Thor <thor1458@gmail.com>
2024-02-27 12:07:59 +0100

    DDPM

---
+++ Part_B/diffusion model.py
@@ -0,0 +1,249 @@
+# Code for DTU course 02460 (Advanced Machine Learning Spring) by Jes Frellsen, 2024
+# Version 1.0 (2024-02-11)
+
+import torch
+import torch.nn as nn
+import torch.distributions as td
+import torch.nn.functional as F
+from tqdm import tqdm
+import sys
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+
+class DDPM(nn.Module):
+    def __init__(self, network, beta_1=1e-4, beta_T=2e-2, T=100):
+        """
+        Initialize a DDPM model.
+
+        Parameters:
+        network: [nn.Module]
+            The network to use for the diffusion process.
+        beta_1: [float]
+            The noise at the first step of the diffusion process.
+        beta_T: [float]
+            The noise at the last step of the diffusion process.
+        T: [int]
+            The number of steps in the diffusion process.
+        """
+        super(DDPM, self).__init__()
+        self.network = network
+        self.beta_1 = beta_1
+        self.beta_T = beta_T
+        self.T = T
+
+        self.beta = nn.Parameter(torch.linspace(beta_1, beta_T, T), requires_grad=False)
+        self.alpha = nn.Parameter(1 - self.beta, requires_grad=False)
+        self.alpha_cumprod = nn.Parameter(self.alpha.cumprod(dim=0), requires_grad=False)
+
+    def negative_elbo(self, x):
+        """
+        Evaluate the DDPM negative ELBO on a batch of data.
+
+        Parameters:
+        x: [torch.Tensor]
+            A batch of data (x) of dimension `(batch_size, *)`.
+        Returns:
+        [torch.Tensor]
+            The negative ELBO of the batch of dimension `(batch_size,)`.
+        """
+
+        # t = td.uniform.Uniform(1, self.T).sample((x.shape)).to(x.device).long()
+        t = td.uniform.Uniform(1, self.T).sample((x.shape)).to(x.device).long()
+        
+        noise = torch.randn_like(x).to(x.device)
+
+        noise = noise / torch.max(noise)
+
+        # print(f"noise : {noise}")
+        # print(f"alpha_cumprod {torch.sqrt(self.alpha_cumprod[t]).shape}")
+        
+        x_t = x * torch.sqrt(self.alpha_cumprod[t]) + noise * torch.sqrt(1 - self.alpha_cumprod[t])
+
+        estimate = self.network(x_t, t)
+
+        # print(f"noise {noise.shape}")
+        # print(f"estimate {estimate.shape}")
+
+        neg_elbo = (noise - estimate) ** 2
+        
+        return neg_elbo
+
+    def sample(self, shape):
+        """
+        Sample from the model.
+
+        Parameters:
+        shape: [tuple]
+            The shape of the samples to generate.
+        Returns:
+        [torch.Tensor]
+            The generated samples.
+        """
+        # Sample x_t for t=T (i.e., Gaussian noise)
+        x_t = torch.randn((shape,784)).to(self.alpha.device)
+
+        # Sample x_t given x_{t+1} until x_0 is sampled
+        for t in range(self.T-1, -1, -1):
+            if t > 0:
+                noise = torch.randn(x_t.shape).to(x_t.device)
+            else:
+                noise = torch.zeros(x_t.shape).to(x_t.device)
+
+            t = (torch.ones(x_t.shape).to(x_t.device) * t).long()
+
+            # print(f"x_t : {x_t.shape}")
+            # print(f"t : {t.shape}")
+
+            ### Implement the remaining of Algorithm 2 here ###
+            x_t = 1.0 / torch.sqrt(self.alpha[t]) * (x_t - self.beta[t] / torch.sqrt(1 - self.alpha_cumprod[t]) * self.network(x_t, t)) + noise * torch.sqrt(self.beta[t])
+            
+        return x_t
+
+    def loss(self, x):
+        """
+        Evaluate the DDPM loss on a batch of data.
+
+        Parameters:
+        x: [torch.Tensor]
+            A batch of data (x) of dimension `(batch_size, *)`.
+        Returns:
+        [torch.Tensor]
+            The loss for the batch.
+        """
+        return self.negative_elbo(x).mean()
+
+
+def train(model, optimizer, data_loader, epochs, device):
+    """
+    Train a Flow model.
+
+    Parameters:
+    model: [Flow]
+       The model to train.
+    optimizer: [torch.optim.Optimizer]
+         The optimizer to use for training.
+    data_loader: [torch.utils.data.DataLoader]
+            The data loader to use for training.
+    epochs: [int]
+        Number of epochs to train for.
+    device: [torch.device]
+        The device to use for training.
+    """
+    model.train()
+
+    total_steps = len(data_loader)*epochs
+    progress_bar = tqdm(range(total_steps), desc="Training")
+
+    for epoch in range(epochs):
+        data_iter = iter(data_loader)
+        for x in data_iter:
+            if isinstance(x, (list, tuple)):
+                x = x[0]
+            x = x.to(device)
+            optimizer.zero_grad()
+            loss = model.loss(x)
+            loss.backward()
+            optimizer.step()
+
+            # Update progress bar
+            progress_bar.set_postfix(loss=f"⠀{loss.item():12.4f}", epoch=f"{epoch+1}/{epochs}")
+            progress_bar.update()
+
+
+class FcNetwork(nn.Module):
+    def __init__(self, input_dim, num_hidden):
+        """
+        Initialize a fully connected network for the DDPM, where the forward function also take time as an argument.
+        
+        parameters:
+        input_dim: [int]
+            The dimension of the input data.
+        num_hidden: [int]
+            The number of hidden units in the network.
+        """
+        super(FcNetwork, self).__init__()
+        # print(f"input dim: {input_dim}")
+        self.network = nn.Sequential(nn.Linear(input_dim*2, num_hidden), nn.ReLU(), 
+                                     nn.Linear(num_hidden, num_hidden), nn.ReLU(), 
+                                     nn.Linear(num_hidden, input_dim))
+
+    def forward(self, x, t):
+        """"
+        Forward function for the network.
+        
+        parameters:
+        x: [torch.Tensor]
+            The input data of dimension `(batch_size, input_dim)`
+        t: [torch.Tensor]
+            The time steps to use for the forward pass of dimension `(batch_size, 1)`
+        """
+        # print(f"x : {x} and t : {t}")
+        x_t_cat = torch.cat([x, t], dim=1)
+        return self.network(x_t_cat)
+
+
+if __name__ == "__main__":
+    import torch.utils.data
+    from torchvision import datasets, transforms
+    from torchvision.utils import save_image
+
+    # Parse arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', type=str, default='train', choices=['train', 'sample', 'test'], help='what to do when running the script (default: %(default)s)')
+    parser.add_argument('--data', type=str, default='tg', choices=['tg', 'cb', 'mnist'], help='dataset to use {tg: two Gaussians, cb: chequerboard} (default: %(default)s)')
+    parser.add_argument('--model', type=str, default='model.pt', help='file to save model to or load model from (default: %(default)s)')
+    parser.add_argument('--samples', type=str, default='samples.png', help='file to save samples in (default: %(default)s)')
+    parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda', 'mps'], help='torch device (default: %(default)s)')
+    parser.add_argument('--batch-size', type=int, default=10000, metavar='N', help='batch size for training (default: %(default)s)')
+    parser.add_argument('--epochs', type=int, default=1, metavar='N', help='number of epochs to train (default: %(default)s)')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='V', help='learning rate for training (default: %(default)s)')
+
+    args = parser.parse_args()
+    print('# Options')
+    for key, value in sorted(vars(args).items()):
+        print(key, '=', value)
+
+    # Load MNIST as binarized at 'thresshold' and create data loaders
+    thresshold = 0.5
+    train_loader = torch.utils.data.DataLoader(datasets.MNIST('data/', train=True, download=True,
+                                                                    transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x)), transforms.Lambda(lambda x: (thresshold < x).float().squeeze())])),
+                                                    batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(datasets.MNIST('data/', train=False, download=True,
+                                                                transform=transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: torch.flatten(x)), transforms.Lambda(lambda x: (thresshold < x).float().squeeze())])),
+                                                    batch_size=args.batch_size, shuffle=True)
+
+    # Get the dimension of the dataset
+    D = next(iter(train_loader))[0][0].shape[0]
+
+    # print(f"D : {D}")
+
+    # Define the network
+    num_hidden = 64
+    network = FcNetwork(D, num_hidden)
+
+    # Set the number of steps in the diffusion process
+    T = 1000
+
+    # Define model
+    model = DDPM(network, T=T).to(args.device)
+
+    # Choose mode to run
+    if args.mode == 'train':
+        # Define optimizer
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+        # Train model
+        train(model, optimizer, train_loader, args.epochs, args.device)
+
+        # Save model
+        torch.save(model.state_dict(), args.model)
+
+    elif args.mode == 'sample':
+        model.load_state_dict(torch.load(args.model, map_location=torch.device(args.device)))
+
+        # Generate samples
+        model.eval()
+        samples = (model.sample(8)).cpu() 
+        save_image(samples.view(8, 1, 28, 28), args.samples)
